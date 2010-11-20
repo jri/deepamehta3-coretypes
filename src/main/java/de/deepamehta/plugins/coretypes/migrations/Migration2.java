c5b661ce5aafd4f4429bb61afd696fdba02645d2
Jörg Richter <jri@deepamehta.de>
2010-11-20 06:42:26 +0100

    Migration 2 fixed (part of upcoming DeepaMehta 3 v0.4.3). All cases are supported: 1) updating from DeepaMehta 3 v0.4, v0.4.1, v0.4.2 2) DeepaMehta 3 v0.4.3 clean installation.

--- src/main/java/de/deepamehta/plugins/coretypes/migrations/Migration2.java
+++ src/main/java/de/deepamehta/plugins/coretypes/migrations/Migration2.java
@@ -11,6 +11,13 @@ import java.util.logging.Logger;
 /**
  * This migration adds a "Search Result" data field to the "Search Result" topic type.
  * <p>
+ * This migration must run ALWAYS and we can *not* put the "Search Result" field in search-result.json.
+ * Consider these cases:
+ * 1) When CLEAN_INSTALL DeepaMehta 3 v0.4.3 Migration 1 runs and search-result.json *is* processed.
+ * 2) When updating from DeepaMehta 3 v0.4 to v0.4.3 CLEAN_INSTALL is detected.
+ * Migration 1 runs but search-result.json is *not* processed.
+ * 3) When updating from DeepaMehta 3 v0.4.1 to v0.4.3 UPDATE is detected.
+ * <p>
  * Distributed with deepamehta3-coretypes v0.4.2 (introduced with DeepaMehta 3 v0.4.3).
  */
 public class Migration2 extends Migration {
